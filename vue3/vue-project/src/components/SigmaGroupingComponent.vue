9339efe9fdc858441463417fac15508c9a8068ca
ohmy04180 <ohmy0418@mobigen.com>
2024-05-20 17:10:08 +0900

    feat: 코드 리팩토

--- vue3/vue-project/src/components/SigmaGroupingComponent.vue
+++ vue3/vue-project/src/components/SigmaGroupingComponent.vue
@@ -1,10 +1,9 @@
-<!-- 그룹화가 되어있는 1300개의 데이터 -->
 <template>
-  <h3>searchNode: {{ state.searchNode }}</h3>
+  <h3>searchNode: {{ searchNode }}</h3>
   <div class="controls">
     <input
       type="text"
-      v-model="state.searchNode"
+      v-model="searchNode"
       class="text-input"
       placeholder="Search nodes..."
     />
@@ -16,11 +15,10 @@
     <button @click="showCircular">원으로 보이기</button>
     <button @click="arrangeGraph">노드의 흐름대로 정렬</button>
     <p>|</p>
-    <label class="checkbox"
-      ><input type="checkbox" id="filter" v-model="state.change" @change="showHalf($event)" />반만
-      보여줘</label
-    >
-    <p>선택한 Node: {{ state.selectedNode }}</p>
+    <label class="checkbox">
+      <input type="checkbox" id="filter" v-model="change" @change="showHalf($event)" />반만 보여줘
+    </label>
+    <p>선택한 Node: {{ selectedNode }}</p>
   </div>
   <div class="controls" id="controls">
     <h3>스냅샷</h3>
@@ -62,25 +60,21 @@ import { circular } from 'graphology-layout'
 import FileSaver from 'file-saver'
 
 const container = ref<HTMLElement | null>(null)
-const state = reactive({
-  graph: new Graph() as any,
-  sigmaInstance: null as Sigma | null,
-  searchNode: ref<string>(''),
-  change: ref<boolean>(false),
-  hoveredNode: ref<any>(''),
-  hoveredNeighbors: new Set<string>(),
-  selectedNode: null as string | null,
-})
+const graph = new Graph()
+const sigmaInstance = ref<Sigma | null>(null)
+const searchNode = ref<string>('')
+const change = ref<boolean>(false)
+const selectedNode = ref<string | null>(null)
 
 let cancelCurrentAnimation: (() => void) | null = null
 
-onMounted(() => {
+const initializeGraph = () => {
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ가나다라마바사아자차타카파하고노도로보소오조초토포코호'
   const items = ['a', 'b', 'c', 'd']
 
   alphabet.split('').forEach((groupLetter) => {
     const topGroup = `Group${groupLetter}`
-    state.graph.addNode(topGroup, {
+    graph.addNode(topGroup, {
       label: topGroup,
       size: 20,
       color: 'black',
@@ -90,7 +84,7 @@ onMounted(() => {
 
     for (let i = 1; i <= 4; i++) {
       const groupItem = `${groupLetter}-${i}`
-      state.graph.addNode(groupItem, {
+      graph.addNode(groupItem, {
         label: groupItem,
         size: 10,
         color: 'gray',
@@ -98,30 +92,34 @@ onMounted(() => {
         y: Math.random() * 100
       })
 
-      state.graph.addEdge(topGroup, groupItem, {
+      graph.addEdge(topGroup, groupItem, {
         label: `Edge-${i}`,
         size: 2
       })
 
       items.forEach((item) => {
         const nodeId = `${groupItem}-${item}`
-        state.graph.addNode(nodeId, {
+        graph.addNode(nodeId, {
           label: nodeId,
           size: 5,
           color: 'lightgray',
           x: Math.random() * 100,
           y: Math.random() * 100
         })
-        state.graph.addEdge(groupItem, nodeId, {
+        graph.addEdge(groupItem, nodeId, {
           label: `Edge-${i}`,
           size: 2
         })
       })
     }
   })
+}
+
+onMounted(() => {
+  initializeGraph()
 
   if (container.value) {
-    state.sigmaInstance = new Sigma(state.graph, container.value, {
+    sigmaInstance.value = new Sigma(graph, container.value, {
       renderEdgeLabels: true,
       allowInvalidContainer: true,
       defaultEdgeColor: '#ccc',
@@ -130,42 +128,39 @@ onMounted(() => {
       labelSize: 15
     })
 
-    state.sigmaInstance?.on('enterNode', (event) => {
+    sigmaInstance.value?.on('enterNode', (event) => {
       resetColors()
       highlightNodeAndNeighbors(event.node)
-      state.sigmaInstance?.refresh()
+      sigmaInstance.value?.refresh()
     })
 
-    state.sigmaInstance?.on('leaveNode', () => {
+    sigmaInstance.value?.on('leaveNode', () => {
       resetColors()
-      state.sigmaInstance?.refresh()
+      sigmaInstance.value?.refresh()
     })
 
-    // TODO 노드 클릭 시 url 이동 이벤트 (추후 데이터를 재겁색 하는 기능으로 바꿀 수 있을듯)
-    state.sigmaInstance?.on('clickNode', (event) => {
-      const nodeId = event.node
-      state.selectedNode = nodeId
+    sigmaInstance.value?.on('clickNode', (event) => {
+      selectedNode.value = event.node
     })
   }
-  console.log(state.graph.nodes())
 
   showRandom()
 })
 
 // 노드 컬러 초기화
 const resetColors = () => {
-  state.graph.forEachNode((node: string) => {
+  graph.forEachNode((node: string) => {
     const originalColor = node.includes('-')
       ? node.split('-').length === 2
         ? 'gray'
         : 'lightgray'
       : 'black'
-    state.graph.updateNodeAttribute(node, 'color', () => originalColor)
+    graph.updateNodeAttribute(node, 'color',() =>  originalColor)
   })
 
-  state.graph.forEachEdge((edge: string) => {
-    state.graph.updateEdgeAttribute(edge, 'color', () => '#ccc')
-    state.graph.updateEdgeAttribute(edge, 'hidden', () => false)
+  graph.forEachEdge((edge: string) => {
+    graph.updateEdgeAttribute(edge, 'color', () => '#ccc')
+    graph.updateEdgeAttribute(edge, 'hidden', () => false)
   })
 }
 
@@ -179,78 +174,68 @@ const highlightNodeAndNeighbors = (nodeId: string) => {
     const currentNode = stack.pop()!
     if (!visitedNodes.has(currentNode)) {
       visitedNodes.add(currentNode)
-      state.graph.updateNodeAttribute(currentNode, 'color', () =>
-        currentNode === nodeId ? 'red' : 'orange'
-      )
+      graph.updateNodeAttribute(currentNode, 'color', () => currentNode === nodeId ? 'red' : 'orange')
 
-      state.graph.forEachNeighbor(currentNode, (neighbor: string) => {
+      graph.forEachNeighbor(currentNode, (neighbor: string) => {
         if (!visitedNodes.has(neighbor)) {
           stack.push(neighbor)
         }
       })
 
-      state.graph.forEachEdge(currentNode, (edge: string) => {
+      graph.forEachEdge(currentNode, (edge: string) => {
         visitedEdges.add(edge)
-        state.graph.updateEdgeAttribute(edge, 'color', () => 'blue')
-        state.graph.updateEdgeAttribute(edge, 'hidden', () => false)
+        graph.updateEdgeAttribute(edge, 'color', () => 'blue')
+        graph.updateEdgeAttribute(edge, 'hidden', () => false)
       })
     }
   }
 
-  state.graph.forEachEdge((edge: string) => {
+  graph.forEachEdge((edge: string) => {
     if (!visitedEdges.has(edge)) {
-      state.graph.updateEdgeAttribute(edge, 'hidden', () => true)
+      graph.updateEdgeAttribute(edge, 'hidden',() =>  true)
     }
   })
 }
 
 // 데이터 검색
 const searchData = () => {
-  const searchLower = state.searchNode.toLowerCase()
+  const searchLower = searchNode.value.toLowerCase()
 
   if (searchLower.includes('edge-')) {
-    state.graph.forEachEdge((edge: any, attributes: any) => {
-      const data = state.graph.getEdgeAttributes(edge)
+    graph.forEachEdge((edge: any, attributes: any) => {
+      const data = graph.getEdgeAttributes(edge)
       if (attributes.label.toLowerCase().includes(searchLower)) {
-        state.graph.setEdgeAttribute(edge, 'color', 'skyblue')
+        graph.setEdgeAttribute(edge, 'color', 'skyblue')
       } else {
-        state.graph.setEdgeAttribute(
-          edge,
-          'hidden',
-          data.label.toLowerCase().indexOf(searchLower) === -1
-        )
+        graph.setEdgeAttribute(edge, 'hidden', data.label.toLowerCase().indexOf(searchLower) === -1)
       }
     })
   } else {
-    state.graph.forEachNode((node: any, attributes: any) => {
-      const data = state.graph.getNodeAttributes(node)
+    graph.forEachNode((node: any, attributes: any) => {
+      const data = graph.getNodeAttributes(node)
       if (attributes.label.toLowerCase().includes(searchLower)) {
-        state.graph.setNodeAttribute(node, 'color', 'red')
+        graph.setNodeAttribute(node, 'color', 'red')
       } else {
-        state.graph.setNodeAttribute(
-          node,
-          'hidden',
-          data.label.toLowerCase().indexOf(searchLower) === -1
-        )
+        graph.setNodeAttribute(node, 'hidden', data.label.toLowerCase().indexOf(searchLower) === -1)
       }
     })
   }
 
-  state.sigmaInstance?.refresh()
+  sigmaInstance.value?.refresh()
 }
 
 // 노드, 엣지 초기화
 const resetGraph = () => {
-  state.graph.forEachNode((node: any) => {
-    state.graph.setNodeAttribute(node, 'hidden', false)
+  graph.forEachNode((node: any) => {
+    graph.setNodeAttribute(node, 'hidden', false)
   })
 
-  state.graph.forEachEdge((edge: any, attributes: any, source: string, target: string) => {
-    state.graph.setEdgeAttribute(edge, 'hidden', false)
-    state.graph.setEdgeAttribute(edge, 'color', '#ccc')
+  graph.forEachEdge((edge: any, attributes: any, source: string, target: string) => {
+    graph.setEdgeAttribute(edge, 'hidden', false)
+    graph.setEdgeAttribute(edge, 'color', '#ccc')
   })
-  state.searchNode = ''
-  state.sigmaInstance?.refresh()
+  searchNode.value = ''
+  sigmaInstance.value?.refresh()
 }
 
 // png 저장
@@ -259,10 +244,12 @@ const saveAsPng = () => {
     (id) => !!(document.getElementById(`layer-${id}`) as HTMLInputElement).checked
   )
 
-  settingPng(state.sigmaInstance, layers)
+  settingPng(sigmaInstance.value, layers)
 }
 
-const settingPng = (renderer: Sigma, inputLayers?: string[]) => {
+const settingPng = (renderer: Sigma | null, inputLayers?: string[]) => {
+  if (!renderer) return
+
   const { width, height } = renderer.getDimensions()
 
   const pixelRatio = window.devicePixelRatio || 1
@@ -275,22 +262,17 @@ const settingPng = (renderer: Sigma, inputLayers?: string[]) => {
   tmpRoot.style.bottom = '101%'
   document.body.appendChild(tmpRoot)
 
-  // Copy camera and force to render now, to avoid having to wait the schedule /
-  // debounce frame:
   renderer.getCamera().setState(renderer.getCamera().getState())
   renderer.refresh()
 
-  // Create a new canvas, on which the different layers will be drawn:
   const canvas = document.createElement('CANVAS') as HTMLCanvasElement
   canvas.setAttribute('width', width * pixelRatio + '')
   canvas.setAttribute('height', height * pixelRatio + '')
   const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 
-  // Draw a white background first:
   ctx.fillStyle = '#fff'
   ctx.fillRect(0, 0, width * pixelRatio, height * pixelRatio)
 
-  // For each layer, draw it on our canvas:
   const canvases = renderer.getCanvases()
   const layers = inputLayers ? inputLayers.filter((id) => !!canvases[id]) : Object.keys(canvases)
   layers.forEach((id) => {
@@ -307,11 +289,9 @@ const settingPng = (renderer: Sigma, inputLayers?: string[]) => {
     )
   })
 
-  // Save the canvas as a PNG image:
   canvas.toBlob((blob) => {
     if (blob) FileSaver.saveAs(blob, 'graph.png')
 
-    // Cleanup:
     renderer.refresh()
     tmpRoot.remove()
   }, 'image/png')
@@ -323,32 +303,32 @@ const showRandom = () => {
 
   const xExtents = { min: 0, max: 0 }
   const yExtents = { min: 0, max: 0 }
-  state.graph.forEachNode((_node: any, attributes: any) => {
+  graph.forEachNode((_node: any, attributes: any) => {
     xExtents.min = Math.min(attributes.x, xExtents.min)
     xExtents.max = Math.max(attributes.x, xExtents.max)
     yExtents.min = Math.min(attributes.y, yExtents.min)
     yExtents.max = Math.max(attributes.y, yExtents.max)
   })
   const randomPositions: PlainObject<PlainObject<number>> = {}
-  state.graph.forEachNode((node: any) => {
+  graph.forEachNode((node: any) => {
     randomPositions[node] = {
       x: Math.random() * (xExtents.max - xExtents.min),
       y: Math.random() * (yExtents.max - yExtents.min)
     }
   })
-  cancelCurrentAnimation = animateNodes(state.graph, randomPositions, { duration: 1500 })
+  cancelCurrentAnimation = animateNodes(graph, randomPositions, { duration: 1500 })
 }
 
 // 원으로 보여주기
 const showCircular = () => {
   if (cancelCurrentAnimation) cancelCurrentAnimation()
 
-  const circularPositions = circular(state.graph, { scale: 100 })
-  cancelCurrentAnimation = animateNodes(state.graph, circularPositions, { duration: 1500 })
+  const circularPositions = circular(graph, { scale: 100 })
+  cancelCurrentAnimation = animateNodes(graph, circularPositions, { duration: 1500 })
 }
 
 const arrangeGraph = () => {
-  if (!state.graph || !state.sigmaInstance) return
+  if (!graph || !sigmaInstance.value) return
 
   const xSpacing = 300
   const ySpacing = 150
@@ -356,18 +336,18 @@ const arrangeGraph = () => {
 
   const positions: { [key: string]: { x: number; y: number } } = {}
 
-  state.graph.forEachNode((node: string, attr: any) => {
+  graph.forEachNode((node: string, attr: any) => {
     if (node.startsWith('Group')) {
       const rootNode = node
       let xOffset = 0
       positions[rootNode] = { x: xOffset, y: yOffset }
 
-      const children = state.graph.neighbors(rootNode)
+      const children = graph.neighbors(rootNode)
       children.forEach((child: any) => {
         xOffset += xSpacing
         positions[child] = { x: xOffset, y: yOffset + ySpacing }
 
-        const grandChildren = state.graph.neighbors(child)
+        const grandChildren = graph.neighbors(child)
         let grandChildOffset = xOffset - ((grandChildren.length / 2) * xSpacing) / 2
         grandChildren.forEach((grandChild: any) => {
           grandChildOffset += xSpacing / 2
@@ -380,31 +360,33 @@ const arrangeGraph = () => {
     }
   })
 
-  cancelCurrentAnimation = animateNodes(state.graph, positions, { duration: 2000 })
+  cancelCurrentAnimation = animateNodes(graph, positions, { duration: 2000 })
 }
 
-// 반만 보이도록 필터 (추후 데이터 카테고리 별로 필터 기능 구현 가능)
+// 반만 보이도록 필터
 const showHalf = (event: any) => {
-  let isChecked = event.target.checked
-  state.graph.nodes().forEach((node: any) => {
-    state.graph.setNodeAttribute(node, 'hidden', isChecked && Math.random() > 0.5)
+  const isChecked = event.target.checked
+  graph.nodes().forEach((node: any) => {
+    graph.setNodeAttribute(node, 'hidden', isChecked && Math.random() > 0.5)
   })
 }
 
 // 확대
 const zoomIn = () => {
-  state.sigmaInstance?.getCamera().animatedZoom({ duration: 400 })
+  sigmaInstance.value?.getCamera().animatedZoom({ duration: 400 })
 }
 
 // 축소
 const zoomOut = () => {
-  state.sigmaInstance?.getCamera().animatedUnzoom({ duration: 400 })
+  sigmaInstance.value?.getCamera().animatedUnzoom({ duration: 400 })
 }
 
 // 확대/축소 초기화
 const zoomReset = () => {
-  state.sigmaInstance?.getCamera().animatedReset({ duration: 600 })
+  sigmaInstance.value?.getCamera().animatedReset({ duration: 600 })
 }
 </script>
 
-<style></style>
+<style scoped>
+/* 스타일 정의 */
+</style>
